6d02c814077df11b9fbd914e9048dc85331564fd
NicolasXd36MlM <yukiminko519@gmail.com>
2024-07-09 14:41:36 -0300

    Criação de metodo AtualizarSetas

--- ProvaDaniel/Assets/UIManager.cs
+++ ProvaDaniel/Assets/UIManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UImanager : MonoBehaviour
 {
     #region Singleton
-    public static UImanager instance;
-
+    public static UIManager instance;
 
     private void Awake()
     {
@@ -16,16 +17,42 @@ public class UImanager : MonoBehaviour
 
 
 
+
     [SerializeField] Sprite sprites();
     [SerializeField] Image imagens();
     [SerializeField] TextMeshProUGUI textoDePontuacao();
     [SerializeField] TextMeshProUGUI textoDoRelogio();
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        // Método público para atualizar as setas
+        public void AtualizarSetas(setas.Keycode)
+        {
+            // Loop para cada imagem no array imagens
+            {
+                // Se o índice for maior ou igual ao comprimento do array setas
+                {
+                    // Define a sprite como a primeira do array sprites
+                }
+                // Se a tecla for DownArrow
+                {
+                    // Define a sprite como a segunda do array sprites
+                }
+                // Se a tecla for UpArrow
+                {
+                    // Define a sprite como a terceira do array sprites
+                }
+                // Se a tecla for LeftArrow
+                {
+                    // Define a sprite como a quarta do array sprites
+                }
+                // Se a tecla for RightArrow
+                {
+                    // Define a sprite como a quinta do array sprites
+                }
+
+                // Define a cor da imagem como branca
+            }
+        }
     }
 
     // Update is called once per frame
